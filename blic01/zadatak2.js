230f832ed8c7a573ff11f9c1c3e83d273d2d9733
Matej <mmuller@student.unipu.hr>
2022-10-11 08:40:01 +0200

    Rjesen zadatak 2

---
+++ blic01/zadatak2.js
@@ -0,0 +1,9 @@
+function funkcija(string1){
+    
+    let chunks = string1.split("nije");
+
+alert(chunks[0]); /* When I say */
+alert(chunks[1]); /* I want to split */
+}
+
+funkcija("Javascript i nije toliko los")
